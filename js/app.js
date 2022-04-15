6811563e3914506e204cedd45481f49c744d4d30
br3n <brenobl2000@gmail.com>
2022-04-15 17:36:54 -0300

    Criação do script de 'ver mais'

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+
+function verMais() { 
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("resumo-more");
+    var btnMoreText = document.getElementById("resumo-click-more");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnMoreText.innerHTML = "ver mais";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnMoreText.innerHTML = "ver menos";
+        moreText.style.display = "inline";
+    }
+}
