9bf5c1c2e959441ab04fef7a0c1a9cf17f94dc10
br3n <brenobl2000@gmail.com>
2022-04-16 22:19:39 -0300

    Criaçao da funcionalidade de criar tópico

--- js/app.js
+++ js/app.js
@@ -5,13 +5,52 @@ function verMais() {
     var moreText = document.getElementById("resumo-more");
     var btnMoreText = document.getElementById("resumo-click-more");
 
-    if (dots.style.display === "none") {
+    if (dots.style.display === "none"){
         dots.style.display = "inline";
         btnMoreText.innerHTML = "ver mais";
         moreText.style.display = "none";
-    } else {
+    }else{
         dots.style.display = "none";
         btnMoreText.innerHTML = "ver menos";
         moreText.style.display = "inline";
+    }
+}
+
+function criarTopico() {
+    var antes = document.getElementById("discus-default");
+    var criarTopico = document.getElementById("topic-forms");
+
+    if(criarTopico.style.display = "none"){
+        criarTopico.style.display = "block";
+        antes.style.display = "none";
+    }else{
+        criarTopico.style.display = "none";
+        antes.style.display = "block";
+    }
+}
+
+function enviarTopico() {
+    var antes = document.getElementById("topic-forms");
+    var feedback = document.getElementById("topic-envio");
+
+    if(feedback.style.display = "none"){
+        feedback.style.display = "block";
+        antes.style.display = "none";
+    }else{
+        feedback.style.display = "none";
+        antes.style.display = "block";
+    }
+}
+
+function outroTopico() {
+    var antes = document.getElementById("topic-envio");
+    var outroTopico = document.getElementById("topic-forms");
+
+    if(outroTopico.style.display = "none"){
+        outroTopico.style.display = "block";
+        antes.style.display = "none";
+    }else{
+        outroTopico.style.display = "none";
+        antes.style.display = "block";
     }
 }
